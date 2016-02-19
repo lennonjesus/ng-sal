10fb48c216779e815ea322a907bbbfa604a937f8
Lennon Jesus <lennon.jesus@gmail.com>
2016-02-19 17:51:46 -0200

    addind currentUser objetc to

--- src/login.service.js
+++ src/login.service.js
@@ -69,6 +69,7 @@
           if (!currentUser) currentUser = {username: data.username, permissions: {}, userDetails: {}};
           currentUser.userDetails = data.userDetails;
           currentUser.permissions = data.permissions;
+          $rootScope.currentUser = currentUser;
           $rootScope.$broadcast('event:userDetailsPrepared');
       };
 
